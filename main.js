76f1723e09c43ef069cf6ce9e2a53b6914b811f3
1x55 <k4i@pm.me>
2023-09-12 11:02:49 +0900

    added async function to main.js

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 // async function
+document.querySelector('button').addEventListerner('click', apiRequest)
 
 async function apiRequest() {
-    
+    const alienName = document.querySelector('input').value
+    try {
+         const response = await fetch(`https://galatic-trek.cyclic.app/api/${alienName}`)
+         const data = await res.json()
+         console.log(data)
+    } catch(err) {
+         console.log(err)
+    }
 }
